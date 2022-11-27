b0824c51cf285a1b65b648b50b1761e6057611c0
Bolun Ma <mabl02@rd.netease.com>
2022-11-27 23:49:19 +0800

    bugfix:拦截器

--- MyBatis-Agent-AutoConfigration/src/main/java/boylen/agent/mybatis/agent/autoconfigration/config/AutoConfiguration.java
+++ MyBatis-Agent-AutoConfigration/src/main/java/boylen/agent/mybatis/agent/autoconfigration/config/AutoConfiguration.java
@@ -3,7 +3,6 @@ package boylen.agent.mybatis.agent.autoconfigration.config;
 import boylen.agent.mybatis.agent.autoconfigration.properties.AgentPropertiesE;
 import boylen.agent.mybatis.agent.autoconfigration.properties.DataSourceProperties;
 import boylen.agent.mybatis.agent.core.core.DataSourceAgent;
-import boylen.agent.mybatis.agent.core.interceptor.AgentInterceptor;
 import boylen.agent.mybatis.agent.core.properties.AgentProperties;
 import boylen.agent.mybatis.agent.core.service.ConfigAgentService;
 import boylen.agent.mybatis.agent.core.service.InitAgentService;
@@ -14,7 +13,6 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -30,7 +28,7 @@ import java.util.Map;
 
 
 @Configuration
-@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE) // 希望我们的自动配置类优先于其他自动配置候选类
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
 @EnableConfigurationProperties({AgentPropertiesE.class, DataSourceProperties.class})
 public class AutoConfiguration {
     @Autowired
@@ -44,7 +42,6 @@ public class AutoConfiguration {
      */
     @Bean
     public DataSource dataSourceAgent() {
-        // 提取配置文件内容
         Map<String, DataSource> dataSourceMap = getDataSourceMapByProperties();
         return new DataSourceAgent(dataSourceMap);
     }
@@ -73,19 +70,15 @@ public class AutoConfiguration {
     @Bean
     public SqlSessionFactory sqlSessionFactory(MybatisConfiguration configuration, DataSource dataSourceAgent) throws Exception {
         MybatisSqlSessionFactoryBean sessionFactoryBean = new MybatisSqlSessionFactoryBean();
-        // DataSource 我注入的其实是我自己写的DataSourceAgent
         sessionFactoryBean.setDataSource(dataSourceAgent);
-        // mapper
         Resource[] resources = agentProperties.initMapperLocations();
         if (resources.length > 0) {
             sessionFactoryBean.setMapperLocations(resources);
         }
-        // Interceptor拦截器
         Interceptor[] interceptors = agentProperties.initInterceptors();
-        if (interceptors.length > 0){
+        if (interceptors.length > 0) {
             sessionFactoryBean.setPlugins(interceptors);
         }
-        // Configuration
         sessionFactoryBean.setConfiguration(configuration);
         return sessionFactoryBean.getObject();
     }
@@ -126,7 +119,6 @@ public class AutoConfiguration {
         String[] password = dataSourceProperties.getPassword();
         String[] driverClassName = dataSourceProperties.getDriverClassName();
         int length = Math.max(Math.max(jdbcUrl.length, username.length), Math.max(password.length, driverClassName.length));
-        // 生成DataSource
         Map<String, DataSource> dataSourceMap = new HashMap<>();
         for (int i = 0; i < length; i++) {
             HikariConfig dataSourceConfig = new HikariConfig();
@@ -134,14 +126,11 @@ public class AutoConfiguration {
             dataSourceConfig.setUsername(username[username.length == 1 ? 0 : i]);
             dataSourceConfig.setPassword(password[password.length == 1 ? 0 : i]);
             dataSourceConfig.setDriverClassName(driverClassName[driverClassName.length == 1 ? 0 : i]);
-            HikariDataSource dataSource = new HikariDataSource(dataSourceConfig); // 数据源连接池
-            // 获取数据库的名字
+            HikariDataSource dataSource = new HikariDataSource(dataSourceConfig);
             String databaseName = getDatabaseNameFromJdbcUrl(jdbcUrl[jdbcUrl.length == 1 ? 0 : i]);
-            // 空值检查
             if (databaseName == null) {
                 continue;
             }
-            // 存储DataSource
             dataSourceMap.put(databaseName, dataSource);
         }
         return dataSourceMap;
